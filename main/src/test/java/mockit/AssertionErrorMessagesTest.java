deaabde697794c1d53e6d6022ba35015a588b934
Jeremy Landis <jeremylandis@hotmail.com>
2025-01-03 23:03:36 -0500

    Move some tests over to junit 5 assert throws

--- main/src/test/java/mockit/AssertionErrorMessagesTest.java
+++ main/src/test/java/mockit/AssertionErrorMessagesTest.java
@@ -7,6 +7,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.rules.ExpectedException;
+import org.junit.jupiter.api.Assertions;
 
 /**
  * The Class AssertionErrorMessagesTest.
