54722b2e44999e3af6a73ab6443ddb15228389ba
Jeremy Landis <jeremylandis@hotmail.com>
2025-04-26 19:25:43 -0400

    [samples] Move petclinic to jakarta faces

--- samples/petclinic/src/main/java/petclinic/vets/VetScreen.java
+++ samples/petclinic/src/main/java/petclinic/vets/VetScreen.java
@@ -4,7 +4,7 @@ import edu.umd.cs.findbugs.annotations.Nullable;
 
 import java.util.List;
 
-import javax.faces.view.ViewScoped;
+import jakarta.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.transaction.Transactional;
