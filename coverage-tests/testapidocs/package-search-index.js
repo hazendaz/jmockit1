10d1b445a3a0dccba0decafe3fe91a69f6dc40b0
hazendaz <jeremylandis@hotmail.com>
2025-10-26 01:51:14 +0000

    Deploying to gh-pages from @ hazendaz/jmockit1@5b999e00b635af8cca212380c5437b1dc94dafec 🚀

--- coverage-tests/testapidocs/package-search-index.js
+++ coverage-tests/testapidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"integrationTests"},{"l":"integrationTests.data"},{"l":"integrationTests.loops"},{"l":"integrationTests.otherControlStructures"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"integrationTests"}];updateSearchResults();
