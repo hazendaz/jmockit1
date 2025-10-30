96c075b82278633f1017a1c34371c52a00481bc8
Jeremy Landis <jeremylandis@hotmail.com>
2025-10-30 13:08:24 -0400

    Use primative boolean instead of Boolean as not needed for the cast

--- main/src/main/java/mockit/asm/constantPool/ConstantPoolGeneration.java
+++ main/src/main/java/mockit/asm/constantPool/ConstantPoolGeneration.java
@@ -447,7 +447,7 @@ public final class ConstantPoolGeneration {
         }
 
         if (cst instanceof Boolean) {
-            int val = (Boolean) cst ? 1 : 0;
+            int val = (boolean) cst ? 1 : 0;
             return newInteger(val);
         }
 
